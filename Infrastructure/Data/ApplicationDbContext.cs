9d2274d8c5df0150bae7d6c70ea2209c5283ac22
Andrei Barabas <barabas.andrei@gmail.com>
2024-05-21 21:47:39 +0300

    Added boilerplate for webapi project infrastructure and domain

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "InMemDb");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
